af4fa8787d0df5a02afa3f85da063e14f4dd40c0
atishoff012803 <kumarsahuatish280301@gmail.com>
2024-01-12 00:16:33 +0530

    Commit - 09
    Context API 02

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,13 +1,10 @@
-import React from 'react';
+import React,{ useContext} from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../Context/AuthContext';
-
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);  
   return (
-    <AuthContext.Consumer>
-    {(ctx)=>{
-      return (
         <nav className={classes.nav}>
       <ul>
         {ctx.isLoggedIn && (
@@ -27,10 +24,6 @@ const Navigation = (props) => {
         )}
       </ul>
     </nav>
-      );
-    }}
-    
-    </AuthContext.Consumer>
   );
 };
 
